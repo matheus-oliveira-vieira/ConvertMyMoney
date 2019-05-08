d09be9dd044200dfb2f3974bb584dba938acba6c
madov68 <matheus.oliveirav@gmail.com>
2019-05-08 20:07:21 -0300

    Adicionando códigos pra funcionar no ZEIT

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const convert = require ('./lib/convert')
-
+const port = process.env.PORT || 3000
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 app.use(express.static(path.join(__dirname, 'public')))
@@ -29,7 +29,7 @@ app.get('/cotacao', (req, res) =>{
 })
 
 
-app.listen(3000, err =>{
+app.listen(port, err =>{
     if(err){
         console.log('Não foi possível iniciar')
     }else{
