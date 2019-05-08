261271b5e95703d168dbc443af519b6686126bbe
madov68 <matheus.oliveirav@gmail.com>
2019-05-08 19:42:51 -0300

    Testando Sourcetree

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.get('/', (req, res) =>{
     res.render('home')
 })
-//teste
+//testedegit
 app.get('/cotacao', (req, res) =>{
     const {cotacao, quantidade} = req.query
     if(cotacao && quantidade){
